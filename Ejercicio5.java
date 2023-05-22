817a24abc16ea44bb03d4feab15323b287f97151
Guoliang Li <gandresli910@gmail.com>
2023-05-22 08:28:43 -0600

    Segunda actualizacion

---
+++ Ejercicio5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Ejercicio5 {
+    public static void main(String[] args) {
+    
+    //numero
+    int n = 5;
+   //Almacenar el factorial
+     int factorial = 1; 
+
+    //Calcular el Factorial
+    int i=1;
+    while (i<=n)
+    
+    //factorial = factorial * i i++;
+    System.out.print(n+"! = "+factorial);
+    
+    
+       }
+    }
