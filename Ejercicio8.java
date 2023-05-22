a4707461021306151e4c7616401eb8350096c4b6
Guoliang Li <gandresli910@gmail.com>
2023-05-22 11:59:18 -0600

    Tercer avance

---
+++ Ejercicio8.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+import javax.management.BadAttributeValueExpException;
+public class Ejerciciosiete {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+     
+      System.out.println("Ingrese un numero: ");
+         int a = entrada.nextInt();
+         while (a <= 0){
+            System.out.println("Ingrese un numero entero: ");
+            a = input.nextInt();
+         }
+         System.out.println("Ingrese un numero: ");
+         int b = entrada.nextInt();
+         while (b <= 0){
+            System.out.println("Ingrese un numero entero: ");
+            a = input.nextInt();
+         }
+         
+         }
+        
+        }
+    
