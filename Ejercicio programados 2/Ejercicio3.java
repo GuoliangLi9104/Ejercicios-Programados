d704cef3202e3c82eaaa6a4c3080aa5f9058e5d3
Guoliang Li <gandresli910@gmail.com>
2023-05-23 19:41:20 -0600

    Reorganizacion de archivos

--- Ejercicio programados 2/Ejercicio3.java
+++ Ejercicio programados 2/Ejercicio3.java
@@ -5,19 +5,24 @@ public class Ejerciciosiete {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
      
-      System.out.println("Ingrese un numero: ");
+      System.out.println("Ingrese el primer numero: ");
          int a = entrada.nextInt();
          while (a <= 0){
             System.out.println("Ingrese un numero entero: ");
-            a = input.nextInt();
+            a = entrada.nextInt();
          }
-         System.out.println("Ingrese un numero: ");
+         System.out.println("Ingrese el segundo numero: ");
          int b = entrada.nextInt();
          while (b <= 0){
             System.out.println("Ingrese un numero entero: ");
-            a = input.nextInt();
+            b = entrada.nextInt();
          }
          
+         if (a > b){
+            System.out.println(a + " Es mayor que " + b);
+         }else{
+            System.out.println(b + " Es mayor que " + a);
+         }
          }
         
         }
